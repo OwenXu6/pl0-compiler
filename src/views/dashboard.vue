46b9d7945c0834b2e38122c612ccba382d47dfe3
IkMangMok <2051828@tongji.edu.cn>
2023-09-05 21:36:17 +0800

    冲突解决

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -73,42 +73,6 @@
 							<el-button style="float: right; padding: 3px 0" text @click="addTodo">添加</el-button>
 						</div>
 					</template>
-<<<<<<< HEAD
-
-					<div class="todo-list-container">
-						<el-table :show-header="false" :data="todoList" style="width: 100%">
-							<el-table-column width="40">
-								<template #default="scope">
-									<el-checkbox v-model="scope.row.status"></el-checkbox>
-								</template>
-							</el-table-column>
-							<el-table-column>
-								<template #default="scope">
-									<div class="todo-item" :class="{ 'todo-item-del': scope.row.status }">
-										{{ scope.row.title }}
-									</div>
-									<el-button @click="viewTodo(scope.row)" size="mini">查看</el-button>
-								</template>
-							</el-table-column>
-						</el-table>
-					</div>
-				</el-card>
-
-			</el-col>
-		</el-row>
-		<el-row :gutter="20">
-			<el-col :span="12">
-				<el-card shadow="hover">
-					<schart ref="bar" class="schart" canvasId="bar" :options="options"></schart>
-				</el-card>
-			</el-col>
-			<el-col :span="12">
-				<el-card shadow="hover">
-					<schart ref="line" class="schart" canvasId="line" :options="options2"></schart>
-				</el-card>
-			</el-col>
-		</el-row>
-=======
 					<el-table :show-header="false" :data="todoList" style="width: 100%">
 						<el-table-column width="40">
 							<template #default="scope">
@@ -129,17 +93,11 @@
 			<!-- </el-col> -->	
 		
 		<el-row><dynamicChart :inData="inAndOut.inData" :outData="inAndOut.outData"></dynamicChart></el-row>
->>>>>>> 443d7dc27f862417685bf326ea2ecc7e3fb587f3
 
 	</div>
 </template>
 
 <script setup lang="ts" name="dashboard">
-<<<<<<< HEAD
-import Schart from 'vue-schart';
-=======
-
->>>>>>> 443d7dc27f862417685bf326ea2ecc7e3fb587f3
 import { reactive, ref, watch } from 'vue';
 import imgurl from '../assets/img/img.jpg';
 import { CENTERED_ALIGNMENT } from 'element-plus/es/components/virtual-list/src/defaults';
@@ -155,92 +113,11 @@ const viewTodo = (todo) => {
 
 const name = localStorage.getItem('ms_username');
 const role: string = name === 'admin' ? '超级管理员' : '普通用户';
-<<<<<<< HEAD
-const vis = reactive({
-	btn1vis: true,
-	btn2vis: false,
-	startTime: false,
-	endTime: true,
-});
-const timer1 = ref(0); // 创建一个引用变量来存储定时器
-const realTime = () => {
-	//unWatch();
-	if (vis.endTime === true) {
-		vis.endTime = false;
-		vis.startTime = true;
-		timer1.value = setInterval(myFunction, 1000); // 每隔一秒触发 myFunction 函数
-	}
-	else {
-		vis.endTime = true;
-		vis.startTime = false;
-		clearInterval(timer1.value);
-	}
-	//clearInterval(timer1.value);
-}
-const myFunction = () => {
-	// 在这里编写你要执行的代码
-	//updateChart();
-	//options4.datasets[0].data = [inAndOut.inData-inAndOut.outData, inAndOut.totData-(inAndOut.inData-inAndOut.outData)];
-	let num1 = 18;
-	let num2 = 15;
-	///var nowData=inAndOut.inData-inAndOut.outData;
-
-	let nowData = inAndOut.inData - inAndOut.outData;
-	if (nowData < 600) {
-		inAndOut.inData += Math.floor(Math.random() * (num1 - 10) + 10);
-		inAndOut.outData += Math.floor(Math.random() * (num2 - 10) + 10);
-	}
-	else {
-		inAndOut.inData += Math.floor(Math.random() * (num2 - 10) + 10);
-		inAndOut.outData += Math.floor(Math.random() * (num1 - 10) + 10);
-	}
-};
-const inAndOut = reactive({
-	inData: 1000,
-	outData: 500,
-	maxPeople: 5000,
-	flag: 0,
-	li1: [{ date: "7.2", inData1: 0 },
-	{ date: "7.3", inData1: 2000 },
-	{ date: "7.4", inData1: 1500 },
-	{ date: "7.5", inData1: 1800 },
-	{ date: "7.6", inData1: 2000 },
-	{ date: "7.7", inData1: 1500 },
-	{ date: "7.8", inData1: 0 },
-	{ date: "7.9", inData1: 2000 },
-	{ date: "7.10", inData1: 1500 },
-	{ date: "7.11", inData1: 1800 },
-	{ date: "7.12", inData1: 2000 },
-	{ date: "7.13", inData1: 1500 },
-	{ date: "7.14", inData1: 2000 },
-	{ date: "7.15", inData1: 1500 },]
-
-
-});
-const options4 = reactive({
-	type: 'ring',
-	title: {
-		text: ''
-	},
-	showValue: true,
-	legend: {
-		position: 'bottom',
-		bottom: 5
-	},
-	bgColor: '#fff',
-	labels: ['在馆人数', '馆内剩余容量'],
-	datasets: [
-		{
-			data: [inAndOut.inData - inAndOut.outData, inAndOut.maxPeople - (inAndOut.inData - inAndOut.outData)],
-		}
-	],
-=======
 
 const inAndOut = reactive({
 	inData: 0,
 	outData: 0,
 	onSet:0,
->>>>>>> 443d7dc27f862417685bf326ea2ecc7e3fb587f3
 });
 let inCount = 0;
 let outCount = 0;
@@ -257,60 +134,6 @@ setInterval(() => {
 	inAndOut.onSet += inAndOut.inData - inAndOut.outData;
 }, 1000);
 
-<<<<<<< HEAD
-const unWatch = watch(inAndOut, () => {
-	updateChart();
-}, { deep: true });
-const updateChart = () => {
-	// 在这里更新图表数据
-	options4.datasets[0].data = [inAndOut.inData - inAndOut.outData, inAndOut.maxPeople - (inAndOut.inData - inAndOut.outData)];
-	//options1.datasets[0].data = [inAndOut.li1[0].inData1, inAndOut.li1[1].inData1,inAndOut.li1[2].inData1, inAndOut.li1[3].inData1, inAndOut.li1[4].inData1,inAndOut.li1[5].inData1];
-};
-const options = {
-	type: 'bar',
-	title: {
-		text: '最近一周各品类销售图'
-	},
-	xRorate: 25,
-	labels: ['周一', '周二', '周三', '周四', '周五'],
-	datasets: [
-		{
-			label: '家电',
-			data: [234, 278, 270, 190, 230]
-		},
-		{
-			label: '百货',
-			data: [164, 178, 190, 135, 160]
-		},
-		{
-			label: '食品',
-			data: [144, 198, 150, 235, 120]
-		}
-	]
-};
-const options2 = {
-	type: 'line',
-	title: {
-		text: '最近几个月各品类销售趋势图'
-	},
-	labels: ['6月', '7月', '8月', '9月', '10月'],
-	datasets: [
-		{
-			label: '家电',
-			data: [234, 278, 270, 190, 230]
-		},
-		{
-			label: '百货',
-			data: [164, 178, 150, 135, 160]
-		},
-		{
-			label: '食品',
-			data: [74, 118, 200, 235, 90]
-		}
-	]
-};
-=======
->>>>>>> 443d7dc27f862417685bf326ea2ecc7e3fb587f3
 const todoList = reactive([
 	{
 		title: '今天要修复100个bug',
@@ -467,9 +290,4 @@ const todoList = reactive([
 	color: white;
 	font-size: 15px;
 
-<<<<<<< HEAD
-}
-</style>
-=======
 }</style>
->>>>>>> 443d7dc27f862417685bf326ea2ecc7e3fb587f3
