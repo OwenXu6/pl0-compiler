f0cd1cbc5cb0261f2ba8056cd99121b74b9d4546
IkMangMok <2051828@tongji.edu.cn>
2023-09-05 21:37:24 +0800

    Revert "Merge branch 'front_end_merge' of https://github.com/Tj-jiaoao/DBMS_museum into front_end_merge"
    This reverts commit 5995efc91aa0fe9d1313eae155d3f2ece64f9ed4, reversing
    changes made to 46b9d7945c0834b2e38122c612ccba382d47dfe3.

--- src/views/collection_add.vue
+++ src/views/collection_add.vue
@@ -1,7 +1,6 @@
 <template>
 	<div>
 		<div class="cardContainer" id="container">
-<<<<<<< Updated upstream
 
 			<el-descriptions class="margin-top" title="藏品编目卡" :column="2" :size="size" border>
 				<!-- 收藏单位 -->
@@ -265,415 +264,22 @@
 				</el-descriptions-item>
 			</el-descriptions>
 
-=======
-
-			<el-form label-width="70px">
-
-				<el-descriptions class="margin-top" :column="2" :size="size" border>
-					<!-- 收藏单位 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item" :style="{ minWidth: form.collectInfo.collectMuseum.length * 12 + 'px' }">
-								<el-icon :style="iconStyle">
-									<user />
-								</el-icon>
-								收藏单位
-							</div>
-						</template>
-						{{ form.collectInfo.collectMuseum }}
-					</el-descriptions-item>
-					<!-- 现登记号 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<EditPen />
-								</el-icon>
-								现登记号
-							</div>
-						</template>
-						{{ form.collectionId }}
-					</el-descriptions-item>
-
-					<!-- 藏品图片 改一个提交方式：从本地上传-->
-					<el-descriptions-item :span="2">
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Picture />
-								</el-icon>
-								藏品图片
-							</div>
-						</template>
-						<el-upload class="upload-demo" action="https://smms.app/api/v2/upload"
-							:headers="{ Authorization: '14ac5499cfdd2bb2859e4476d2e5b1d2bad079bf' }" :on-success="handleSuccess"
-							:on-error="handleError" :before-upload="beforeUpload">
-							<el-button size="small" type="primary">点击上传</el-button>
-						</el-upload>
-						<!-- 显示已上传的图片 -->
-						<el-image v-if="form.collectionPhoto" class="CollectionImg" :src="form.collectionPhoto"
-							:z-index="10">
-						</el-image>
-						<!--el-input v-model="form.collectionPhoto"></el-input-->
-						<!--template #default="scope">
-							<el-image class="CollectionImg" :src="form.collectionPhoto" :z-index="10">
-							</el-image>
-						</template-->
-					</el-descriptions-item>
-
-					<!-- 藏品音频 改一个提交方式：从本地上传-->
-					<el-descriptions-item :span="2">
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Picture />
-								</el-icon>
-								藏品音频
-							</div>
-						</template>
-						<el-input v-model="form.collectionAudio"></el-input>
-						<!--template #default="scope">
-							<el-image class="CollectionImg" :src="form.collectionAudio" :z-index="10">
-							</el-image>
-						</template-->
-					</el-descriptions-item>
-
-					<!-- 名称 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<tickets />
-								</el-icon>
-								名称
-							</div>
-						</template>
-						<el-input v-model="form.name"></el-input>
-					</el-descriptions-item>
-					<!-- 文物原名 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<tickets />
-								</el-icon>
-								原名
-							</div>
-						</template>
-						<el-input v-model="form.originalName"></el-input>
-					</el-descriptions-item>
-					<!-- 文物级别 select没写完-->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Medal />
-								</el-icon>
-								文物级别
-							</div>
-						</template>
-						<el-autocomplete v-model="form.collectInfo.collectionLevel" :fetch-suggestions="eraQuerySearch"
-							clearable class="inline-input w-50" placeholder="请输入文物级别" @select="protectLevelSelect" />
-					</el-descriptions-item>
-					<!-- 文物类别 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Grid />
-								</el-icon>
-								文物类别
-							</div>
-						</template>
-						<el-input v-model="form.collectionType"></el-input>
-					</el-descriptions-item>
-					<!-- 质地 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Pointer />
-								</el-icon>
-								质地
-							</div>
-						</template>
-						<el-autocomplete v-model="form.textureType" :fetch-suggestions="typeQuerySearch" clearable
-							class="inline-input w-50" placeholder="请输入文物的种类" @select="typeHandleSelect" />
-					</el-descriptions-item>
-					<!--年代 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Calendar />
-								</el-icon>
-								年代
-							</div>
-						</template>
-						<el-autocomplete v-model="form.era" :fetch-suggestions="eraQuerySearch" clearable
-							class="inline-input w-50" placeholder="请输入文物的年代" @select="eraHandleSelect" />
-					</el-descriptions-item>
-					<!-- 地域 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<LocationInformation />
-								</el-icon>
-								地域
-							</div>
-						</template>
-						<el-input v-model="form.area"></el-input>
-					</el-descriptions-item>
-					<!-- 来源 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<MapLocation />
-								</el-icon>
-								来源
-							</div>
-						</template>
-						<el-input v-model="form.collectInfo.source"></el-input>
-					</el-descriptions-item>
-					<!-- 保存状况 就是当前状况吗？-->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Collection />
-								</el-icon>
-								保存状况
-							</div>
-						</template>
-						<!-- <el-input v-model="form.storageInfo.currentStatus"></el-input> -->
-						<el-radio-group v-model="form.storageInfo.currentStatus">
-							<el-radio label="在展">在展</el-radio>
-							<el-radio label="在库">在库</el-radio>
-							<el-radio label="未鉴定">未鉴定</el-radio>
-						</el-radio-group>
-
-					</el-descriptions-item>
-
-					<!-- 完残程度 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Box />
-								</el-icon>
-								完残程度
-							</div>
-						</template>
-						<el-input v-model="form.completeness"></el-input>
-					</el-descriptions-item>
-
-					<!-- 完残程度类别 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Box />
-								</el-icon>
-								完残程度类别
-							</div>
-						</template>
-						<el-input v-model="form.completenessType"></el-input>
-					</el-descriptions-item>
-
-					<!-- 尺寸 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<FullScreen />
-								</el-icon>
-								尺寸
-							</div>
-						</template>
-						<el-input v-model="form.dimensionInfo.dimension"></el-input>
-					</el-descriptions-item>
-
-					<!-- 尺寸单位 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<FullScreen />
-								</el-icon>
-								尺寸单位
-							</div>
-						</template>
-						<el-input v-model="form.dimensionInfo.dimensionUnit"></el-input>
-					</el-descriptions-item>
-
-					<!-- 质量-->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Coin />
-								</el-icon>
-								质量
-							</div>
-						</template>
-						<el-input v-model="form.dimensionInfo.weight"></el-input>
-					</el-descriptions-item>
-					<!-- 质量单位-->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Coin />
-								</el-icon>
-								质量单位
-							</div>
-						</template>
-						<el-input v-model="form.dimensionInfo.weightUnit"></el-input>
-					</el-descriptions-item>
-					<!-- 传统数量 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Histogram />
-								</el-icon>
-								传统数量
-							</div>
-						</template>
-						<el-input v-model="form.dimensionInfo.traditionalQuantity"></el-input>
-					</el-descriptions-item>
-					<!-- 传统数量单位 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Histogram />
-								</el-icon>
-								传统数量单位
-							</div>
-						</template>
-						<el-input v-model="form.dimensionInfo.traditionalQuantityUnit"></el-input>
-					</el-descriptions-item>
-
-					<!-- 实际数量 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Histogram />
-								</el-icon>
-								实际数量
-							</div>
-						</template>
-						<el-input v-model="form.dimensionInfo.realQuantity"></el-input>
-					</el-descriptions-item>
-
-					<!-- 实际数量单位 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Histogram />
-								</el-icon>
-								实际数量单位
-							</div>
-						</template>
-						<el-input v-model="form.dimensionInfo.realQuantityUnit"></el-input>
-					</el-descriptions-item>
-
-					<!-- 保护等级 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Trophy />
-								</el-icon>
-								保护等级
-							</div>
-						</template>
-						<el-autocomplete v-model="form.storageInfo.protectionLevel" :fetch-suggestions="eraQuerySearch"
-							clearable class="inline-input w-50" placeholder="请输入文物的保护等级" @select="protectLevelSelect" />
-					</el-descriptions-item>
-
-					<!-- 备注 -->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<Histogram />
-								</el-icon>
-								备注
-							</div>
-						</template>
-						<el-input v-model="form.remark"></el-input>
-					</el-descriptions-item>
-
-
-					<!-- 入藏时间 自动给生成？-->
-					<el-descriptions-item>
-						<template #label>
-							<div class="cell-item">
-								<el-icon :style="iconStyle">
-									<AlarmClock />
-								</el-icon>
-								入藏时间
-							</div>
-						</template>
-						<el-input v-model="form.collectInfo.collectTime"></el-input>
-					</el-descriptions-item>
-
-				</el-descriptions>
-
-				<el-form-item>
-					<el-button type="primary" @click="submitForm(form)">
-						提交
-					</el-button>
-					<!--el-button @click="resetForm(form)">重置</el-button-->
-				</el-form-item>
-
-			</el-form>
-
-
->>>>>>> Stashed changes
 		</div>
 	</div>
 </template>
 
 <script setup lang="ts" name="basetable">
-import { ref, reactive, computed } from 'vue';
+import { ref, reactive } from 'vue';
 import { ElMessage, ElMessageBox } from 'element-plus';
 import { Delete, Edit, Search, Plus } from '@element-plus/icons-vue';
-<<<<<<< Updated upstream
 import { fetchData } from '../api/index';
-=======
-import {
-	Iphone,
-	Location,
-	OfficeBuilding,
-	Tickets,
-	User,
-} from '@element-plus/icons-vue'
-
-//import { fetchData } from '../api/index';
->>>>>>> Stashed changes
 import { onMounted } from 'vue'
 import axios from 'axios';
 import type { FormInstance, FormRules } from 'element-plus'
 
-<<<<<<< Updated upstream
 let tmp = ({
 	location: "",
 })
-=======
-
-
-let tmp = ({
-	location: "",
-})
-
->>>>>>> Stashed changes
 
 interface TableItem {
 	collectionId: number,      //ID
@@ -685,10 +291,6 @@ interface TableItem {
 	textureType: string,       //质地类别（例如有机质类、无机质类）
 	completeness: string,      //完残程度
 	completenessType: string,  //完残程度类别：完、残、缺、失
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 	collectionPhoto: string,   //图片
 	collectionAudio: string,   //音频
 
@@ -696,10 +298,7 @@ interface TableItem {
 		collectionId: number,  //ID
 		source: string,        //来源，如旧藏、拨交、移交
 		collectMuseum: string, //收藏博物馆
-<<<<<<< Updated upstream
 		generalRegistrationId: string,  //总登记号，不是说总登记号即Collection_id?
-=======
->>>>>>> Stashed changes
 		collectTime: string,            //入藏时间
 		collectionLevel: string         //藏品级别
 	},
@@ -711,13 +310,8 @@ interface TableItem {
 		weightUnit: string,         //质量单位，默认为克
 		realQuantity: number,       //实际数量
 		realQuantityUnit: string,   //实际数量单位，默认为件
-<<<<<<< Updated upstream
 		traditionalQuantity: number //传统数量
 		//where is 传统数量单位
-=======
-		traditionalQuantity: number, //传统数量
-		traditionalQuantityUnit: string//传统数量单位
->>>>>>> Stashed changes
 	},
 	storageInfo: {                  //保存组
 		collectionId: number,       //ID
@@ -727,10 +321,6 @@ interface TableItem {
 	exhibitionHallId: number,  //展厅ID
 	warehouseId: number,       //仓库ID
 	containerId: number,       //库架ID
-<<<<<<< Updated upstream
-=======
-
->>>>>>> Stashed changes
 	identificationStaffName: string, //鉴别人员名称
 	identificationComments: string,  //鉴别结果
 	identificationDate: string,      //鉴别日期
@@ -738,11 +328,7 @@ interface TableItem {
 }
 
 let newCollection: TableItem = {
-<<<<<<< Updated upstream
 	collectionId: 4,
-=======
-	collectionId: 99,
->>>>>>> Stashed changes
 	originalName: "string",
 	name: "string",
 	collectionType: "12",
@@ -754,7 +340,6 @@ let newCollection: TableItem = {
 	collectionPhoto: "string",
 	collectionAudio: "string",
 	collectInfo: {
-<<<<<<< Updated upstream
 		"collectionId": 4,
 		"source": "string",
 		"collectMuseum": "string",
@@ -764,57 +349,30 @@ let newCollection: TableItem = {
 	},
 	dimensionInfo: {
 		collectionId: 4,
-=======
-		collectionId: 99,
-		source: "string",
-		collectMuseum: "string",
-		collectTime: "2023-09-03T00:00:00",
-		collectionLevel: "string"
-	},
-	dimensionInfo: {
-		collectionId: 99,
->>>>>>> Stashed changes
 		dimension: "string",
 		dimensionUnit: "string",
 		weight: 0,
 		weightUnit: "string",
 		realQuantity: 0,
 		realQuantityUnit: "string",
-<<<<<<< Updated upstream
 		traditionalQuantity: 0
 	},
 	storageInfo: {
 		collectionId: 4,
-=======
-		traditionalQuantity: 0,
-		traditionalQuantityUnit: "string"
-	},
-	storageInfo: {
-		collectionId: 99,
->>>>>>> Stashed changes
 		currentStatus: "未鉴定",
 		protectionLevel: "string"
 	},
 	exhibitionHallId: null,
-<<<<<<< Updated upstream
 	warehouseId: 1,
 	containerId: 1,
 	identificationStaffName: "jiaoao",
 	identificationComments: "string",
 	identificationDate: "2023-09-03T07:46:58.924",
-=======
-	warehouseId: null,
-	containerId: null,
-	identificationStaffName: "1212",
-	identificationComments: "string",
-	identificationDate: "2023-09-03T07:46:58",
->>>>>>> Stashed changes
 	remark: "string"
 
 };
 
 const form = reactive({
-<<<<<<< Updated upstream
 	collectionId: 4,
 	originalName: "string",
 	name: "string",
@@ -858,51 +416,6 @@ const form = reactive({
 	remark: "string"
 
 })
-=======
-	collectionId: 777,
-	originalName: "",
-	name: "",
-	collectionType: "",
-	era: "",
-	area: "",
-	textureType: "",
-	completeness: "",
-	completenessType: "",
-	collectionPhoto: "",
-	collectionAudio: "",
-	collectInfo: {
-		collectionId: 777,
-		source: "",
-		collectMuseum: "苏州博物馆",
-		collectTime: "2023-09-03T00:00:00",//这里要么就是 yyyy-mm-ddT+时间(**:**:**)，要么就是只有日期yyyy-mm-dd
-		collectionLevel: ""
-	},
-	dimensionInfo: {
-		collectionId: 777,
-		dimension: "",
-		dimensionUnit: "",
-		weight: null,
-		weightUnit: "",
-		realQuantity: null,
-		realQuantityUnit: "",
-		traditionalQuantity: null,
-		traditionalQuantityUnit: ""
-	},
-	storageInfo: {
-		collectionId: 777,
-		currentStatus: "",
-		protectionLevel: ""
-	},
-	exhibitionHallId: null,
-	warehouseId: null,
-	containerId: null,
-	identificationStaffName: "焦骜",
-	identificationComments: "string",
-	identificationDate: "2023-09-03T07:46:58",
-	remark: ""
-
-});
->>>>>>> Stashed changes
 
 const rules = {
 	/*CollectionID: [
@@ -975,26 +488,11 @@ const query = reactive({
 });
 const tableData = ref<TableItem[]>([]);
 const pageTotal = ref(0);
-
-//获取后端数据库的数据
-const fetchData = async () => {
-	try {
-		const response = await axios.get(' http://42.192.39.198:5000/api/Collections');
-		console.log(response.data);
-		console.log("数据库连接成功！");
-		return response.data;
-	} catch (error) {
-		console.error(error);
-	}
-};
-
+// 获取表格数据
 const getData = () => {
 	fetchData().then(res => {
-		//自动生成collectionId
-		form.collectionId = Object.keys(res).length + 1;
-		form.collectInfo.collectionId = form.collectionId;
-		form.storageInfo.collectionId = form.collectionId;
-		form.dimensionInfo.collectionId = form.collectionId;
+		tableData.value = res.data.list;
+		pageTotal.value = res.data.pageTotal || 50;
 	});
 };
 getData();
@@ -1064,33 +562,10 @@ const typeHandleSelect = (item: TypeSelectItem) => {
 	console.log(item)
 }
 
-<<<<<<< Updated upstream
-=======
-
-//可选择的选项
-const protectLevelLoadAll = () => {
-	return [
-		{ value: '一级', index: 1 },
-		{ value: '两级', index: 2 },
-		{ value: '三级', index: 3 },
-	]
-}
-//处理选择的项，比如说给一个东西赋值
-const protectLevelSelect = (item: EraSelectItem) => {
-	console.log(item)
-}
-
->>>>>>> Stashed changes
 onMounted(() => {
 	toSelect.value = typeLoadAll()
 })
 
-<<<<<<< Updated upstream
-=======
-
-const size = ref('')
-
->>>>>>> Stashed changes
 // 文物时期下拉菜单的属性
 interface EraSelectItem {
 	value: string
@@ -1167,76 +642,8 @@ const handleDelete = (index: number) => {
 		.catch(() => { });
 };
 
-<<<<<<< Updated upstream
-=======
-const iconStyle = computed(() => {
-	const marginMap = {
-		large: '8px',
-		default: '6px',
-		small: '4px',
-	}
-	return {
-		marginRight: marginMap.default,
-	}
-})
-
-
-// 在组件创建时获取当前时间并填充到 collectTime
-form.collectInfo.collectTime = getCurrentTime();
-
-function getCurrentTime() {
-	const now = new Date();
-	const year = now.getFullYear();
-	const month = (now.getMonth() + 1).toString().padStart(2, '0');
-	const day = now.getDate().toString().padStart(2, '0');
-	const hours = now.getHours().toString().padStart(2, '0');
-	const minutes = now.getMinutes().toString().padStart(2, '0');
-	const seconds = now.getSeconds().toString().padStart(2, '0');
-	return `${year}-${month}-${day}T${hours}:${minutes}:${seconds}`;
-}
-
-// 处理上传成功后的响应
-/*function handleSuccess(response, file) {
-	// 在这里获取上传成功后的图片地址，并更新 form.collectionPhoto
-	if (response && response.code === 0) { // 根据实际返回数据判断上传是否成功
-		form.collectionPhoto = response.data.imageUrl; // 假设响应中包含了图片的地址
-	}
-}*/
-
-/*const upload = (file) => {
-	const formData = new FormData();
-	formData.append("smfile", file.file);
-	axios
-		.post("https://sm.ms/api/v2/", formData, {
-			headers: {
-				"Content-Type": "multipart/form-data",
-				"Authorization": "14ac5499cfdd2bb2859e4476d2e5b1d2bad079bf",
-			},
-		})
-		.then((res) => {
-			console.log(res);
-		});
-};*/
-
-
-const handleSuccess = (response, file) => {
-      // 处理上传成功后的回调
-      console.log('上传成功', response);
-	  form.collectionPhoto=response.data.url;
-      // 在回调中处理SM.MS返回的数据，可以获取图片链接等信息
-};
-const handleError = (err) => {
-      // 处理上传失败后的回调
-      console.error('上传失败', err);
-};
-const beforeUpload = (file) => {
-      // 在上传前可以执行一些操作，例如限制文件类型、大小等
-      return true; // 返回true表示继续上传，返回false表示取消上传
-};
->>>>>>> Stashed changes
 
 </script>
-
 
 <style scoped>
 .handle-box {
